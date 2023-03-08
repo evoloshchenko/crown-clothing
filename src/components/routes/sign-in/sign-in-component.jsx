3e0fdcb8907a77792e7a9fed73622667610b7da0
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-08 18:20:06 +0200

    feat: add `Sign-In` component and functionality for it using `firebase` and log in with Google.

---
+++ src/components/routes/sign-in/sign-in-component.jsx
@@ -0,0 +1,17 @@
+import { signInwithGooglePopup } from "../../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInwithGooglePopup();
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
