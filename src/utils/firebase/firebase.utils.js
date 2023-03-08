4be0047645abe759d313094f477eb7886de120f4
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-08 18:21:07 +0200

    feat: add config info for `auth` and `signInwithGooglePopup`.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNg5qkruWKL0nchEDxhzHEmV9dND5nZEw",
+  authDomain: "crown-clothing-db-343ea.firebaseapp.com",
+  projectId: "crown-clothing-db-343ea",
+  storageBucket: "crown-clothing-db-343ea.appspot.com",
+  messagingSenderId: "968108384836",
+  appId: "1:968108384836:web:c34fa3df8e830f5ad9d211",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInwithGooglePopup = () => signInWithPopup(auth, provider);
