99dc65d3c0c67dd798055b605d2c9d4771e2e2fd
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-09 20:12:44 +0200

    feat: add `createUserDocumentFromAuth` that write the user to our database in Firestore.

--- src/components/routes/sign-in/sign-in-component.jsx
+++ src/components/routes/sign-in/sign-in-component.jsx
@@ -1,9 +1,12 @@
-import { signInwithGooglePopup } from "../../../utils/firebase/firebase.utils";
+import {
+  signInwithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInwithGooglePopup();
-    console.log(response);
+    const { user } = await signInwithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBNg5qkruWKL0nchEDxhzHEmV9dND5nZEw",
   authDomain: "crown-clothing-db-343ea.firebaseapp.com",
@@ -26,3 +28,28 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInwithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const database = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(database, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
