667e6545f39d1d184361a92994c721f79d6ff44c
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-12 17:41:52 +0200

    feat: add reusable `form-input` component.

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,20 @@
+import "./form-input.styles.scss";
+
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <div class="group">
+      <input className="form-input" {...otherProps} />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
