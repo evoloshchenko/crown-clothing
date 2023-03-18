486a51f9fc12a191b0ad310d9cc85f3f20059439
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-18 16:50:23 +0200

    feat: update `CartDropdown` component with `CartContext` functionality.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,27 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../context/cart.context";
+
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
 
+import CartItem from "../cart-item/cart-item.component";
+
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem cartItem={item} key={item.id} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
       <Button>Go to Checkout</Button>
     </div>
-  )
-}
+  );
+};
 
 export default CartDropdown;
