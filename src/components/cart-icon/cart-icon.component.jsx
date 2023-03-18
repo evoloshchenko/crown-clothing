9c76f63c299a57ca1b211bdf5795df38854535a6
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-18 16:51:05 +0200

    feat: `cartCount` using `CartContext` functionality.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,14 @@ import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container">
       <ShopingIcon className="shopping-icon" onClick={toggleIsCartOpen} />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
