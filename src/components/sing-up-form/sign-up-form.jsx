af4a36be480d6d217d07fef64cd55492ebf4523c
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-28 17:36:48 +0300

    feat: change CSS styles for `SignUpForm` component to styled component.

--- src/components/sing-up-form/sign-up-form.jsx
+++ src/components/sing-up-form/sign-up-form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import "./sign-up-form.styles.scss";
+import {} from "./sign-up-form.styles.jsx";
 import Button from "../button/button.component";
 
 import {
