d5f36f2cb2b35d0ed9991ff9b3a1285b18c8a7cb
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-03-28 17:36:36 +0300

    feat: change CSS styles for `SignInForm` component to styled component.

--- src/components/sing-in-form/sign-in-form.jsx
+++ src/components/sing-in-form/sign-in-form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import "./sign-in-form.styles.scss";
+import { SignUpContainer, ButtonsContainer } from "./sign-in-form.styles.jsx";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import {
@@ -17,8 +17,6 @@ const defaultFormFields = {
 const SingInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
-
-  console.log(formFields);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -57,7 +55,7 @@ const SingInForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Already have an account</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -77,7 +75,7 @@ const SingInForm = () => {
           name="password"
           value={password}
         />
-        <div className="buttons-container">
+        <ButtonsContainer>
           <Button type="submit">Sign In</Button>
           <Button
             type="button"
@@ -86,9 +84,9 @@ const SingInForm = () => {
           >
             Google Sign In
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
