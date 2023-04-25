9fba4d7fb9225562c7a588245765bf45d6ca0f98
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2023-04-25 20:05:09 +0300

    refactor: change `user.context` to use reducers than the states.

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -41,7 +42,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
